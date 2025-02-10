33448c396af0e2c8efbf50487b1217b694526a47
Roy Netanel <roy.netanel@kla-tencor.com>
2025-02-10 12:36:45 +0200

    basic exercise

---
+++ exercise/basic_calc.py
@@ -0,0 +1,7 @@
+x = 16
+y = 4
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
---
+++ exercise/basic_circle.py
@@ -0,0 +1,7 @@
+import math
+
+radius = 12
+area = radius*radius*math.pi
+circumference = 2*radius*math.pi
+print ("the area is:", area)
+print ("the circumference is:", circumference)
